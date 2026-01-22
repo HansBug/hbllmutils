772a31d492fddc18793c8b8bfe1fe37fa08eea45
HansBug <hansbug@buaa.edu.cn>
2026-01-22 15:25:51 +0800

    dev(hansbug): use version v0.2.0

--- hbllmutils/config/meta.py
+++ hbllmutils/config/meta.py
@@ -11,7 +11,7 @@ setup configuration and package distribution.
 __TITLE__ = "hbllmutils"
 
 #: Version of this project.
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.2.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = ('A Python utility library for streamlined Large Language Model interactions '
